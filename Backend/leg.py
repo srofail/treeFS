c9d3428f5aecffe19ee649d37e5b9378719662ef
Philip Howard <howardphilip7@gmail.com>
2025-08-30 22:09:39 +1000

    Include file closing, finish bus live tracking

--- Backend/leg.py
+++ Backend/leg.py
@@ -8,19 +8,25 @@ class Leg:
         self.live_data = self.get_live_data()
         self.duration = json["duration"]
         self.distance = 1 # TODO: figure out
-        self.model = "" # TODO: figure out
+        self.model = self.find_model_name()
         self.departureTime = json["origin"]["departureTimeEstimated"]
         self.stops = "" # TODO: figure out
         self.origin = json["origin"]["name"]
         self.destination = json["destination"]["name"]
-        self.occupancy_status = "" # TODO: figure out
+        self.occupancy_status = self.find_occupancy()
         self.name = json["transportation"]["name"]
 
     def find_model_name(self):
-        pass
+        if self.live_data == None:
+            return "Unknown"
+        
+        return self.live_data["vehicle"]["vehicle"]["[transit_realtime.tfnsw_vehicle_descriptor]"]["vehicle_model"] 
 
     def find_occupancy(self):
-        pass
+        if self.live_data == None:
+            return "Unknown"
+        
+        return self.live_data["vehicle"]["occupancy_status"]
 
     def get_live_data(self):
         if self.mode != 5:
@@ -28,17 +34,24 @@ class Leg:
         
         fobj = open("API_KEY", "r")
         api_key = fobj.readline()
+        fobj.close()
         buses_url = 'https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses'
 
         headers = {
             "Authorization": api_key
         }
-        
+
         params = {
             'debug': 'true',
         }
 
         buses_response = requests.get(buses_url, params=params, headers=headers)
+
+        final = None
+
+        for bus in buses_response.json():
+            if bus["vehicle"]["trip"]["trip_id"] == self.id:
+                final = bus
         
-        return buses_response
+        return final
 
