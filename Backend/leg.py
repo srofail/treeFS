fc02219fd1b1630a0156aaa2cd02e6b075a62a6d
Philip Howard <howardphilip7@gmail.com>
2025-08-30 16:18:14 +1000

    Changes Leg parameters

--- Backend/leg.py
+++ Backend/leg.py
@@ -1,12 +1,14 @@
 class Leg:
 
-    def __init__(self, id, duration, mode, vehicle, departureTime, stops, origin, destination, occupancy_status):
+    def __init__(self, id, duration, distance, mode, model, departureTime, stops, origin, destination, occupancy_status):
         self.id = id
         self.duration = duration
+        self.distance = distance
         self.mode = mode
-        self.vehicle = vehicle
+        self.model = model
         self.departureTime = departureTime
         self.stops = stops
         self.origin = origin
         self.destination = destination
+        self.occupancy_status = occupancy_status
 
