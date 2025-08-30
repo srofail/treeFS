b37c17134cbb377484636b50399342e5e8082a94
Philip Howard <howardphilip7@gmail.com>
2025-08-30 16:08:26 +1000

    Created backend basic structuring

---
+++ Backend/journey.py
@@ -0,0 +1,10 @@
+from leg import Leg
+
+class Journey:
+
+    def __init__(self, data: "JSON"):
+        self.parse_journey(data)
+
+    def parse_journey(self, data):
+        # TODO: parse through JSON and turn into a number of legs
+        pass
---
+++ Backend/leg.py
@@ -0,0 +1,12 @@
+class Leg:
+
+    def __init__(self, id, duration, mode, vehicle, departureTime, stops, origin, destination, occupancy_status):
+        self.id = id
+        self.duration = duration
+        self.mode = mode
+        self.vehicle = vehicle
+        self.departureTime = departureTime
+        self.stops = stops
+        self.origin = origin
+        self.destination = destination
+
