eeb3445b044e4f7d76f1aba9871bd2ea615d441d
Philip Howard <howardphilip7@gmail.com>
2025-08-31 11:54:23 +1000

    samc hanges

--- Backend/server.py
+++ Backend/server.py
@@ -6,10 +6,10 @@ app = Flask(__name__, template_folder='../Frontend', static_folder='../static')
 @app.route("/", methods=["GET", "POST"])
 def start_page():
     if request.method == "POST":
-        # origin_latitude = request.form['origin_latitude']
-        # origin_longitude = request.form['origin_longitude']
-        # destination_latitude = request.form['destination_latitude']
-        # destination_longitude = request.form['destination_longitude']
+        # origin_latitude = request.form.get('origin_latitude', 'string')
+        # origin_longitude = request.form.get('origin_longitude', 'string')
+        # destination_latitude = request.form.get('destination_latitude', 'string')
+        # destination_longitude = request.form.get('destination_longitude', 'string')
 
         session['origin_name'] = "University of Sydney"
         session['dest_name'] = "Random Place"
@@ -25,12 +25,24 @@ def list_of_options():
 
     return jsonify({"journey_list": presentation, "start": session["origin_name"], "end": session["dest_name"]})
 
-@app.route("/options")
+@app.route("/options", methods=["GET","POST"])
 def options_page():
     if request.method == "POST":
-        pass # redirect to the chosen journey
+        session['route_chosen'] = request.form['number']
+        print("got it")
+        return redirect(url_for('more_info'))
 
     return render_template("options.html")
+
+@app.route("/list_of_info")
+def list_of_info():
+    presentation = Backend.get_short_formats(Backend.results[session['journeys']][session['route_chosen']])
+
+    return jsonify({"journey_list": presentation, "start": session["origin_name"], "end": session["dest_name"]})
+
+@app.route("/more_info")
+def more_info():
+    return render_template("more_info.html")
 
 if __name__ == "__main__":
 	# Development server entrypoint
